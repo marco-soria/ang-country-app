d5f4298a660a60f961f5670e988a57624a93d01f
marco-soria <marco.soriaa7@gmail.com>
2025-06-22 10:42:32 -0500

    improved navigation , cache and query params

--- src/app/country/components/search-input/search-input.component.ts
+++ src/app/country/components/search-input/search-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, effect, input, linkedSignal, output } from '@angular/core';
 
 @Component({
   selector: 'app-search-input',
@@ -8,5 +8,22 @@ import { Component, input, output } from '@angular/core';
 })
 export class SearchInputComponent {
   placeholder = input('Buscar');
+  debounceTime = input(1000);
+  initialValue = input<string>();
+
   value = output<string>();
+
+  inputValue = linkedSignal<string>(() => this.initialValue() ?? '');
+
+  debounceEffect = effect((onCleanup: (arg0: () => void) => void) => {
+    const value = this.inputValue();
+
+    const timeout = setTimeout(() => {
+      this.value.emit(value);
+    }, this.debounceTime());
+
+    onCleanup(() => {
+      clearTimeout(timeout);
+    });
+  });
 }
